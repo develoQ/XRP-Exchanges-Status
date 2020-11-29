438e0decdbb9313bf1b2fe56daa49c1eb30fdbc9
develoQ <develoQ.jp@gmail.com>
2020-11-29 18:08:54 +0900

    add date

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -19,25 +19,41 @@
             <v-icon color="accent">mdi-twitter</v-icon>
           </v-btn>
         </v-card-title>
-        <v-card-subtitle>{{ exchange.address }}</v-card-subtitle>
+        <v-card-subtitle class="py-1">{{ exchange.address }}</v-card-subtitle>
+        <v-card-subtitle class="py-1">
+          Activated: {{ exchange.activated }}
+        </v-card-subtitle>
         <v-card-text>
-          <v-container class="pb-0">
+          <v-container fluid>
             <v-row>
-              <v-col class="py-2"> 入庫タグ必須 </v-col>
-              <v-col class="py-2">
-                <v-icon v-if="exchange.flag.includes(1)" color="success">
+              <v-col cols="6" class="py-2"> 入庫タグ必須 </v-col>
+              <v-spacer />
+              <v-col cols="2" class="py-2">
+                <v-icon
+                  v-if="existsFlag(exchange, FlagTypeRequireDest)"
+                  color="success"
+                >
                   mdi-check-bold
                 </v-icon>
                 <v-icon v-else color="error">mdi-alert</v-icon>
               </v-col>
+              <v-col cols="4" class="text-center">
+                {{ typeActivatedDate(exchange, FlagTypeRequireDest) }}
+              </v-col>
             </v-row>
             <v-row>
-              <v-col class="py-2"> マスターキー無効化 </v-col>
-              <v-col class="py-2">
-                <v-icon v-if="exchange.flag.includes(4)" color="success">
+              <v-col cols="6" class="py-2"> マスターキー無効化 </v-col>
+              <v-col cols="2" class="py-2">
+                <v-icon
+                  v-if="existsFlag(exchange, FlagTypeDisableMaster)"
+                  color="success"
+                >
                   mdi-check-bold
                 </v-icon>
                 <v-icon v-else color="error">mdi-alert</v-icon>
+              </v-col>
+              <v-col cols="4" class="text-center">
+                {{ typeActivatedDate(exchange, FlagTypeDisableMaster) }}
               </v-col>
             </v-row>
           </v-container>
@@ -50,8 +66,11 @@
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator'
 import { exchanges } from '../store/index'
+import { exchangeDataType, FlagType } from '../store/exchange'
+
 @Component({
   async asyncData() {
+    console.log(exchanges.getExchangeData)
     if (exchanges.getExchangeData.length === 0) {
       await exchanges.fetchExchangesData()
     }
@@ -60,6 +79,36 @@ import { exchanges } from '../store/index'
 export default class extends Vue {
   get exchanges() {
     return exchanges.exchangeData
+  }
+
+  get existsFlag() {
+    return (ex: exchangeDataType, flag: FlagType) => {
+      return ex.flag.find((a) => {
+        return a.type === flag
+      })
+    }
+  }
+
+  get FlagTypeRequireDest() {
+    return FlagType.RequireDest
+  }
+
+  get FlagTypeDisableMaster() {
+    return FlagType.DisableMaster
+  }
+
+  get typeActivatedDate() {
+    return (ex: exchangeDataType, flag: FlagType) => {
+      const filterd = ex.flag.filter((a) => {
+        return a.type === flag
+      })
+      console.log(filterd)
+      if (filterd.length === 0) {
+        return '-'
+      } else {
+        return filterd[0].date
+      }
+    }
   }
 
   get webLink() {
